5019859af11a4f03270d2a9d5f304f77bfad6d56
Prathamesh Daulat Nagare <nagareprathamesh20@gmail.com>
2026-09-03 23:40:48 +0530

    Remove SponsorsSection from App component

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ export default function App() {
       <StatsSection />
       <TimelineSection />
       <PrizesSection />
-      <SponsorsSection />
       <VenueSection />
       <FaqSection />
       <FinalCta />
